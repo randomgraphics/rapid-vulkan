a2245df7d6994423304a69ead5d82016621b6aa5
randomgraphics <gfxrgb@gmail.com>
2023-05-22 14:33:33 -0700

    added an script to verify that rapid-vulkan.h revision is increased with every commit to it

--- dev/bin/cit.py
+++ dev/bin/cit.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
-import sys, subprocess, os
+import sys, subprocess, os, re
 import importlib; utils = importlib.import_module("rapid-vulkan-utils")
 
-def run_style_check():
-    # uncomment to print clang-format version
-    # subprocess.run(["clang-format-14", "--version"])
+def get_header_revision(content):
+    m = re.search(r"#define RAPID_VULKAN_HEADER_REVISION (\d+)", content)
+    return int(m.group(1)) if m else 0
 
+def check_header_revision():
+    sdk_root_dir = utils.get_root_folder()
+    git_remote = subprocess.check_output(["git", "remote"], cwd=sdk_root_dir).decode(sys.stdout.encoding).strip()
+    header_path = "inc/rapid-vulkan/rapid-vulkan.h"
+    header_diff = subprocess.check_output(["git", "diff", "-U0", "--no-color", git_remote + "/main", "--", header_path], cwd=sdk_root_dir).decode(sys.stdout.encoding).strip()
+    if 0 == len(header_diff): return # bail out if the header is not changed.
+
+    # get header revision of the local file
+    with open(sdk_root_dir / header_path, "r") as f: local_revision = get_header_revision(f.read())
+    print(f"Local header revision: {local_revision}")
+
+    # get header revision of the remote file
+    remote_header = subprocess.check_output(["git", "show", git_remote + "/main:" + str(header_path)], cwd=sdk_root_dir).decode(sys.stdout.encoding).strip()
+    remote_revision = get_header_revision(remote_header)
+    print(f"Remote header revision: {remote_revision}")
+
+    # check if the header revision is increased
+    if local_revision <= remote_revision:
+        utils.rip("The header revision is not increased. Please increase the header revision.")
+    else:
+        print("Header revision is increased. All good!")
+
+def run_style_check():
     # get changes from git.
     sdk_root_dir = utils.get_root_folder()
     git_remote = subprocess.check_output(["git", "remote"], cwd=sdk_root_dir).decode(sys.stdout.encoding).strip()
@@ -22,12 +45,11 @@ def run_style_check():
     # check coding style of the diff
     format_diff = subprocess.check_output(cmdline, input=diff, cwd=sdk_root_dir).decode("utf-8")
     if len(format_diff) > 0:
-        utils.loge("The following changes are violating coding style standard:")
-        print(format_diff)
-        sys.exit(-1)
+        utils.rip(f"The following changes are violating coding style standard:\n{format_diff}")
 
     # Done
     print("Style check done. All good!")
 
+check_header_revision()
 run_style_check()
 utils.run_the_latest_binary("dev/test/{variant}/rapid-vulkan-test", sys.argv[1:])
--- inc/rapid-vulkan/rapid-vulkan.h
+++ inc/rapid-vulkan/rapid-vulkan.h
@@ -26,7 +26,7 @@ SOFTWARE.
 #define RAPID_VULKAN_H_
 
 /// A monotonically increasing number that uniquely identify the revision of the header.
-#define RAPID_VULKAN_REVISION 1
+#define RAPID_VULKAN_HEADER_REVISION 1
 
 /// \def RAPID_VULKAN_NAMESPACE
 /// Define the namespace of rapid-vulkan library.
