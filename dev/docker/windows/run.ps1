3a14c73ef19904ab698e91519cf3e3f3fc37fc7a
Chen Li <chen.li@innopeaktech.com>
2023-06-05 18:10:45 -0700

    windows ci pipeline

--- dev/docker/windows/run.ps1
+++
@@ -1,8 +0,0 @@
-# read tag.txt file
-$tag = Get-Content "$PSScriptRoot\tag.txt"
-$tag = $tag -replace '\s', '' # remove space and new line of $tag
-"tag = $tag"
-
-# run docker
-"docker run --rm -it $tag"
-docker run --rm -it $tag
