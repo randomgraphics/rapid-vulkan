9447b1fbc2ad24db619b2872340b8a370c1ec79f
randomgraphics <gfxrgb@gmail.com>
2023-06-26 18:33:13 -0700

    increased header revision

--- inc/rapid-vulkan/rapid-vulkan.h
+++ inc/rapid-vulkan/rapid-vulkan.h
@@ -26,7 +26,7 @@ SOFTWARE.
 #define RAPID_VULKAN_H_
 
 /// A monotonically increasing number that uniquely identify the revision of the header.
-#define RAPID_VULKAN_HEADER_REVISION 15
+#define RAPID_VULKAN_HEADER_REVISION 17
 
 /// \def RAPID_VULKAN_NAMESPACE
 /// Define the namespace of rapid-vulkan library.
